88e8e3140bd0629963c8c92ff48e176b2758aa2e
anthony84 <anthony.vautier84@gmail.com>
2018-09-26 08:57:53 +0200

    initialisation

---
+++ demo.js
@@ -0,0 +1,14 @@
+/*global $ */
+
+$(function(){
+   $(".sidebarbtn").click (function(){
+       $(".sidebar").toggleClass("active");
+       
+});
+
+});
+
+$(function(){
+      $('.slider').bxSlider();
+    });
+
